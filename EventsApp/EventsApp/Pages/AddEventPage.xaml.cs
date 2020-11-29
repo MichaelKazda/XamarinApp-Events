9f5372d7188b7b77075e606066a2e5f8663085e2
dufekja <jan.dufek2002@gmail.com>
2020-11-29 20:56:44 +0100

    addeventpage update + nuget toast plugin

--- EventsApp/EventsApp/Pages/AddEventPage.xaml.cs
+++ EventsApp/EventsApp/Pages/AddEventPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Toast;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,14 @@ namespace EventsApp.Pages {
     public partial class AddEventPage : ContentPage {
         public AddEventPage() {
             InitializeComponent();
+        }
+
+        private void GetImageButton_Clicked(object sender, EventArgs e) {
+            CrossToastPopUp.Current.ShowToastMessage("Image from gallery action");
+        }
+
+        private void AddEventButton_Clicked(object sender, EventArgs e) {
+            CrossToastPopUp.Current.ShowToastMessage("Event added");
         }
     }
 }
