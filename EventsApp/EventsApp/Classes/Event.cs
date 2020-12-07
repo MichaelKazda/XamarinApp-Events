f9fcf812ece133cf4c32c56e3fb97e07e21d9da9
Dufek Jan <jan.dufek2002@gmail.com>
2020-12-07 15:31:16 +0100

    fixed scrollview + event class

---
+++ EventsApp/EventsApp/Classes/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EventsApp.Classes
+{
+    public class Event
+    {
+        private string Name;
+        private string Desc;
+
+        public Event(string _name, string _desc)
+        {
+            Name = _name;
+            Desc = _desc;
+        }
+
+    }
+}
