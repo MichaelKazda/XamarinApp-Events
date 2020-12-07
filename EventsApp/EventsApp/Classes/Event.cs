e0a16dba776eb280a35552078f59835b29d34668
Dufek Jan <jan.dufek2002@gmail.com>
2020-12-07 15:51:29 +0100

    event update

--- EventsApp/EventsApp/Classes/Event.cs
+++ EventsApp/EventsApp/Classes/Event.cs
@@ -6,11 +6,20 @@ namespace EventsApp.Classes
         private string Name;
         private string Note;
         private string Place;
+        private int TimestampFrom;
+        private int TimestampTo;
+        private bool YearlyRepeat;
+        private int YearsCounter;
 
-        public Event(string _name, string _desc)
+        public Event(string _name, string _desc, string _place, int _timestampFrom, int _timestampTo, bool _yearlyRepeat, int _yearsCounter)
         {
             Name = _name;
             Note = _desc;
+            Place = _place;
+            TimestampFrom = _timestampFrom;
+            TimestampTo = _timestampTo;
+            YearlyRepeat = _yearlyRepeat;
+            YearsCounter = _yearsCounter;
         }
 
     }
