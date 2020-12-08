6b6832c4ee7f3c38c18e6ae79ba7151ab068a464
Michael Kazda <58641230+MichaelKazda@users.noreply.github.com>
2020-12-08 16:00:27 +0100

    read and insert

---
+++ EventsApp/EventsApp/Classes/Firestore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Cloud.Firestore;
+
+namespace EventsApp.Classes
+{
+    public static class firestore
+    {
+        private static FirestoreDb db = FirestoreDb.Create();
+
+        public static async System.Threading.Tasks.Task insertEventAsync(Dictionary<string,object> eventData, string userId)
+        {
+            DocumentReference docRef = db.Collection("users").Document(userId);
+            await docRef.SetAsync(eventData);
+        }
+
+        public static async System.Threading.Tasks.Task<Dictionary<string, object>> getUserData(string userId)
+        {
+            DocumentReference userDoc = db.Collection("users").Document(userId);
+            DocumentSnapshot userSnapshot = await userDoc.GetSnapshotAsync();
+            return userSnapshot.ToDictionary();
+        }
+    }
+}
