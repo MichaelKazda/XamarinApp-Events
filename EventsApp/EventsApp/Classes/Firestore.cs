2c81b7274876e967f79e1e2f6568d2dd059099e4
dufekja <jan.dufek2002@gmail.com>
2021-01-13 19:15:14 +0100

    Update Firestore.cs

--- EventsApp/EventsApp/Classes/Firestore.cs
+++ EventsApp/EventsApp/Classes/Firestore.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Google.Cloud.Firestore;
 
 namespace EventsApp.Classes
 {
-    public static class firestore
+    public static class Firestore
     {
         private static FirestoreDb db = FirestoreDb.Create();
 
-        public static async System.Threading.Tasks.Task insertEventAsync(Dictionary<string,object> eventData, string userId)
+        public static async Task insertEventAsync(Dictionary<string,object> eventData, string userId)
         {
             DocumentReference docRef = db.Collection("users").Document(userId);
             await docRef.SetAsync(eventData);
         }
 
-        public static async System.Threading.Tasks.Task<Dictionary<string, object>> getUserData(string userId)
+        public static async Task<Dictionary<string, object>> getUserData(string userId)
         {
             DocumentReference userDoc = db.Collection("users").Document(userId);
             DocumentSnapshot userSnapshot = await userDoc.GetSnapshotAsync();
