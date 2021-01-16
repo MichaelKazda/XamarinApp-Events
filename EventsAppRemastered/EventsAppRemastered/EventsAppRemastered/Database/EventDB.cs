965ace24cf95588e74cbedf1f7c1902faf0991f2
dufekja <jan.dufek2002@gmail.com>
2021-01-16 17:33:22 +0100

    firebase profi init

---
+++ EventsAppRemastered/EventsAppRemastered/EventsAppRemastered/Database/EventDB.cs
@@ -0,0 +1,42 @@
+﻿using EventsApp.Database;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAppRemastered.Database {
+    class EventDB {
+        readonly SQLiteAsyncConnection _DB;
+
+        public EventDB(string dbPath) {
+            _DB = new SQLiteAsyncConnection(dbPath);
+            _DB.CreateTableAsync<Event>().Wait();
+        }
+
+        public async Task<List<Event>> GetNotesAsync() {
+            return await _DB.Table<Event>().ToListAsync();
+        }
+
+        public Task<Event> GetNoteAsync(int id) {
+            return _DB.Table<Event>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveNoteAsync(Event note) {
+            int insertedRows = await _DB.InsertAsync(note);
+            return insertedRows;
+        }
+
+        public async Task EditNoteAsync(int id, string label, string text) {
+            Event note = await GetNoteAsync(id);
+
+        }
+
+        public async void DeleteNoteAsync(Event note) {
+            int deletedRows = await _DB.DeleteAsync(note);
+        }
+    }
+}
+
